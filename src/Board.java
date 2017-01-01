dcae3951201675457994a303cf7ecd8c19ce7f9a
pct960 <pct960@gmail.com>
2017-01-01 20:38:00 +0530

    Added evaluation method

--- src/Board.java
+++ src/Board.java
@@ -77,6 +77,68 @@ public class Board extends Applet
         addMouseMotionListener(this);
     }
 
+    public float evaluation ( )
+    {float value = 0;
+        float figur = 0;
+        for (int i = 21; i < 99; i++)
+        {if ( board [i] != 0 )
+        {switch (board [i] % 10)
+        {case 1:figur = 1.0f;break;
+            case 2:
+            case 3:
+                figur = 3.0f;
+                break;
+            case 4:figur = 4.5f;break;
+            case 5:figur = 9.0f;break;
+            case 6:figur = 0.0f;break;}
+            figur += posvalues [i];
+            if(board [i] % 100  / 10 == color)value += figur;
+            else value -= figur;}
+            if(i%10 == 8)i += 2;}
+        return value;	}
+    public void execute (int start, int end)
+    {board [end] = board [start];
+        board [start] = 0;
+        if (board [end] % 10 == 6)
+        {if(end == start + 2)
+        {board [start + 1] = board [start + 3] % 100;
+            board [start + 3] = 0;
+            graphboard [start + 1] = board [start + 1];
+            graphboard [start + 3] = 0;
+            paintField (start + 3);
+            paintField (start + 1);}
+            if( end == start - 2)
+            {board [start - 1] = board [start - 4] % 100;
+                board [start - 4] = 0;
+                graphboard [start - 1] = board [start - 1];
+                graphboard [start - 4] = 0;
+                paintField (start - 4);
+                paintField (start - 1);}}
+        if ( (board [end] % 10 == 1) && ((end < 29) || (end > 90)) )
+            board [end] += 4;;
+        graphboard [start] = board [start];
+        graphboard [end] = board [end];
+        paintField (end);
+        paintField (start);
+        if (color == 1)
+        {color = 2;
+            th = new Thread (this);
+            th.setPriority (10);
+            th.start ();}
+        else
+        {color = 1;
+            movecounter = 0;
+            deep = 0;
+            target = 1;
+            genmove ();
+            if (movecounter == 0)
+            {if (ischeck ())
+            {infoBox("Chessmaster Wins!","Game Status");
+                destroy();}
+            else
+            {infoBox("Game is a draw!","Game Status");
+                destroy();}}}}
+
     public void newgame() {
     }
 }
